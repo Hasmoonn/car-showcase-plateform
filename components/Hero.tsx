12131a0a9b56c8eda639f5ab70e6943b7722581f
Hasmoonn <mohamedhasmoon175@gmail.com>
2026-02-20 11:35:32 +0530

    add CustomButton comp, add all necessary images and pics in public folder and export index.ts

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import React from 'react'
 import Image from 'next/image';
 
 import next from 'next'
 import CustomButton from './CustomButton';
 const Hero = () => {
+
+  const handleScroll = () => {
+
+  }
   return (
     <div className='hero'>
-      <div className='flex-1 pt-36 padding-x'>
+      <div className='flex-1 padding-x pt-36'>
         <h1 className='hero__title'>
           Find, book, or rent a car - quickly and easily!
         </h1>
@@ -15,7 +21,15 @@ const Hero = () => {
           Streamline your car rental experience with Our effortless booking process.
         </p>
 
-        <CustomButton />
+        <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
+      </div>
+
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image src="/hero.png" alt='hero' fill className='object-contain' />
+
+          <div className='hero__image-overlay'></div>
+        </div>
       </div>
     </div>
   )
