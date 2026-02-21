ff3e8d0afb23694587e22ef8c90d170d43d1221b
Hasmoonn <mohamedhasmoon175@gmail.com>
2026-02-21 21:07:39 +0530

    Carprops interface added

--- types/index.ts
+++ types/index.ts
@@ -4,7 +4,10 @@ export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
-  btnType: "button" | "submit";
+  btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
 
 export interface CustomFilterProps {
@@ -19,3 +22,19 @@ export interface SearchManuFacturerProps {
 export interface CustomFilterProps {
   title: string;
 }
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive:string;
+  fuel_type:string;
+  highway_mpg:number;
+  make:string;
+  model:string;
+  transmission:string;
+  year:number;
+}
+
