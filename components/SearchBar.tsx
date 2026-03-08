b7fb2bc80991adcd4936372ec135776ca447de41
Hasmoonn <mohamedhasmoon175@gmail.com>
2026-03-08 22:17:26 +0530

    trimmed

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -27,7 +27,7 @@ const SearchBar = () => {
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
-    if (manufacturer === "" && model === "") {
+    if (manufacturer.trim() === "" && model.trim() === "") {
       return alert("Please fill in the search bar")
     }
 
